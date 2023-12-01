5a71e083171959a7b4a24c37d83d3f5d5e27c5b2
Edgar-Vargas <vargasedgar16054@gmail.com>
2023-12-01 13:54:24 -0800

    initial project

---
+++ src/main/java/com/Lyrics/demo/MacLyricsApplication.java
@@ -0,0 +1,14 @@
+package com.Lyrics.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacLyricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacLyricsApplication.class, args);
+		System.out.println("test");
+	}
+
+}
